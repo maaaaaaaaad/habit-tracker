151203314c7b2bf1d86101287da2d60fc16cd406
OH DONG WOONG <integral5137@gmail.com>
2021-03-23 21:22:51 +0900

    function memo

--- src/components/habitAddForm.jsx
+++ src/components/habitAddForm.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { memo } from "react";
 
-const HabitAddForm = (props) => {
+const HabitAddForm = memo((props) => {
   const formRef = React.createRef();
   const inputRef = React.createRef();
 
@@ -22,6 +22,6 @@ const HabitAddForm = (props) => {
       <button className="add-button">Add</button>
     </form>
   );
-};
+});
 
 export default HabitAddForm;
